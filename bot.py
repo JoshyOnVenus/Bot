36a6ae92e89c81a915f986e0b421c74f105a2b8e
JoshyOnVenus <astropulseltd@gmail.com>
2024-04-12 14:06:53 +0100

    Commit #7

--- bot.py
+++ bot.py
@@ -49,7 +49,7 @@ class LoggingFormatter(logging.Formatter):
 
     def format(self, record):
         log_color = self.COLORS[record.levelno]
-        format = "(black){asctime}(reset) (levelcolor){levelname:<8}(reset) (green){name}(reset) {message}"
+        format = "(black)[{asctime}](reset) (levelcolor)[{levelname:<8}](reset) (green){name}:(reset) {message}"
         format = format.replace("(black)", self.black + self.bold)
         format = format.replace("(reset)", self.reset)
         format = format.replace("(levelcolor)", log_color)
